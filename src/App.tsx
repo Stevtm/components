75abdb137339b6194d36b570013850fa396887a0
Steven Maddison <steventmaddison@outlook.com>
2021-11-21 21:31:24 -0500

    create landing page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+// page imports
+import Landing from "./pages/Landing";
+
 const App = () => {
     return <>
     <h1>components</h1>
+    <Landing />
     </>
 }
 
---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,7 @@
+const Landing: React.FC = () => {
+    return (
+        <h2>This is the landing page!</h2>
+    )
+}
+
+export default Landing; 
