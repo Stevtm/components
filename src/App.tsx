abfc942a96e24f60fa1614a45ac85a348bafccff
Steven Maddison <steventmaddison@outlook.com>
2021-11-22 21:59:29 -0500

    apply theme provider and global styles to appc

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { ThemeProvider } from "styled-components";
+
+// import theme and global style
+import { GlobalStyles } from "./global";
+import { theme } from "./theme";
+
 // page imports
 import Landing from "./pages/Landing";
 
@@ -5,10 +11,13 @@ import Landing from "./pages/Landing";
 import Nav from "./components/Nav";
 
 const App = () => {
-    return <>
-        < Nav />
-        <Landing />
-    </>
-}
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <Nav />
+            <Landing />
+        </ThemeProvider>
+    );
+};
 
-export default App; 
+export default App;
